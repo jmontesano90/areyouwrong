f7a11ec0518ee2914a1fc5cb78c4396c4a806b7c
Joseph Montesano <jmmmontesano@gmail.com>
2021-04-29 13:14:52 -0400

    added base for animations

--- src/buttons/Accept.js
+++ src/buttons/Accept.js
@@ -2,13 +2,63 @@ import React, { Component } from 'react'
 import './Accept.css'
 
 class Accept extends Component {
+
+    
     render(){
+
+        // let time = Math.random();
+        // let one = document.querySelector('.one');
+        // one.style.setProperty('--animation-time', time + 's');
         return (
             <div>
                 
                 <button className='acceptButton'> </button>
                 <div className="beneath">Accept「Judgement」</div>
-                <div className='beneath2'>Learn the extent of your inaccuracy</div>
+                <div className='beneath2'>
+                    <span className="one">L</span>
+                    <span className="two">e</span>
+                    <span className="three">a</span>
+                    <span className="four">r</span>
+                    <span className="one">n</span>
+                    <span className="zero">_</span>
+                    <span className="two">t</span>
+                    <span className="three">h</span>
+                    <span className="four">e</span>
+                    <span className="zero">_</span>
+                    <span className="one">e</span>
+                    <span className="two">x</span>
+                    <span className="three">t</span>
+                    <span className="four">e</span>
+                    <span className="one">n</span>
+                    <span className="two">t</span>
+                    <span className="zero">_</span>
+                    <span className="one">o</span>
+                    <span className="two">f</span>
+                    <span className="zero">_</span>
+                    <span className="one">y</span>
+                    <span className="two">o</span>
+                    <span className="three">u</span>
+                    <span className="four">r</span>
+                    <span className="zero">_</span>
+                    <span className="one">i</span>
+                    <span className="two">n</span>
+                    <span className="three">a</span>
+                    <span className="four">c</span>
+                    <span className="one">c</span>
+                    <span className="two">u</span>
+                    <span className="three">r</span>
+                    <span className="four">a</span>
+                    <span className="one">c</span>
+                    <span className="two">y</span>
+
+
+                
+                
+                
+                
+                
+                
+                </div>
             
             
             </div>
