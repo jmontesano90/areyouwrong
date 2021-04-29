4c0267a2d8be4c1c4ac1ef5ead0d0720a06725e7
Joseph Montesano <jmmmontesano@gmail.com>
2021-04-29 12:03:16 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Accept from './buttons/Accept'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+
       </header>
+      <div className='intro'>
+      <h1>Your statement has been questioned</h1>
+      <h2>Does your falsehood hold <span>merit?</span></h2>
+      <Accept></Accept>
+      </div>
     </div>
   );
 }
---
+++ src/buttons/Accept.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './Accept.css'
+
+class Accept extends Component {
+    render(){
+        return (
+            <div>
+                
+                <button className='acceptButton'> </button>
+                <div className="beneath">Accept「Judgement」</div>
+                <div className='beneath2'>Learn the extent of your inaccuracy</div>
+            
+            
+            </div>
+         
+        )
+    }
+}
+
+export default Accept;
